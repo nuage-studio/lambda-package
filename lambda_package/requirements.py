eb334d01233c627c8943eb4fb2a53c801ad08eee
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-05-08 14:57:16 +0000

    Normalized Python version number so that patch versions are allowed

--- lambda_package/requirements.py
+++ lambda_package/requirements.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from random import choice
+from re import compile
 from shutil import copy
 from string import ascii_lowercase
 
@@ -12,23 +13,26 @@ temporary directory, either using Docker or using pip on the local machine.
 """
 
 CacheDir = ".lambda-package"
+VersionRegex = compile("(^[0-9]+\\.[0-9]+)(\\.[0-9]+)?$")
 
 
 def build_requirements(configuration: Configuration):
     if configuration.use_docker:
+        # Create a temporary directory for /var/task
         temp_dir = Path(CacheDir).joinpath(generate_temp_task_dir()).absolute()
         temp_dir.mkdir(parents=True)
 
+        # Copy the requirements file into the directory
         requirements_src_path = Path(configuration.requirements)
         requirements_dest_path = temp_dir.joinpath(requirements_src_path.name)
-
         copy(str(requirements_src_path), str(requirements_dest_path))
 
-        vols = {str(temp_dir): {"bind": "/var/task", "mode": "z"}}
-
+        # Launch the Docker instance to install the requirements
         client = from_env()
+        vols = {str(temp_dir): {"bind": "/var/task", "mode": "z"}}
+        python_version = normalize_version(configuration.python_version)
         client.containers.run(
-            f"lambci/lambda:build-python{configuration.python_version}",
+            f"lambci/lambda:build-python{python_version}",
             f"pip install -t /var/task/ -r /var/task/{requirements_dest_path.name}",
             volumes=vols,
         )
@@ -46,3 +50,20 @@ def generate_temp_task_dir():
     random_chars = "".join(choice(ascii_lowercase) for i in range(8))
     timestamp = datetime.now().strftime("%d%m%Y%H%M%S")
     return f"requirements_task_dir_{timestamp}_{random_chars}"
+
+
+def normalize_version(version_string):
+    """
+    Normalize a Python version string to be in the form: `[major].[minor]`, as this
+    is the format required by the Docker instance.  This means the patch number will be
+    discarded if present, and any other format will raise a ValueError.
+
+    """
+    m = VersionRegex.match(version_string)
+
+    if m is None:
+        raise ValueError(
+            f"Invalid Python version: '{version_string}'.  Version must be in the form [major].[minor]"
+        )
+
+    return m.group(1)
---
+++ test/requirements_normalize_version_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from lambda_package.requirements import normalize_version
+
+
+class RequirementsNormalizeVersionTests(unittest.TestCase):
+    def test_when_call_with_valid_version_then_no_change(self):
+        result = normalize_version("5.2")
+        self.assertEqual("5.2", result)
+
+    def test_when_call_with_patch_then_remove_patch(self):
+        result = normalize_version("5.2.6")
+        self.assertEqual("5.2", result)
+
+    def test_when_call_with_major_only_then_error(self):
+        self.assertRaises(ValueError, normalize_version, "5")
+
+    def test_when_call_with_invalid_then_error(self):
+        self.assertRaises(ValueError, normalize_version, "a.b")

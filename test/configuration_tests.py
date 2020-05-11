aa6da8117060fc146897c41421f9d514e1782bae
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-05-11 21:36:19 +0000

    Changed unit tests to use patch decorators on classes instead of methods

--- test/configuration_tests.py
+++ test/configuration_tests.py
@@ -5,12 +5,12 @@ from unittest.mock import Mock
 from lambda_package.configuration import ConfigFileName, Configuration, SetupFileName
 
 
+@mock.patch("lambda_package.Configuration.read_config_dict")
 class ConfigurationTests(unittest.TestCase):
     """
     Unit tests for the `Configuration` class
     """
 
-    @mock.patch("lambda_package.Configuration.read_config_dict")
     def test_create_from_config_file(self, read_config_dict_mock: Mock):
         read_config_dict_mock.return_value = {
             "output": "my_output",
@@ -21,7 +21,6 @@ class ConfigurationTests(unittest.TestCase):
         self.assertEqual(configuration.output, "my_output")
         self.assertEqual(configuration.exclude, ["my_exclude1", "my_exclude2"])
 
-    @mock.patch("lambda_package.Configuration.read_config_dict")
     def test_when_rc_file_has_output_and_setup_file_doesnt_then_config_has_rc_value(
         self, read_config_dict_mock: Mock
     ):
@@ -36,7 +35,6 @@ class ConfigurationTests(unittest.TestCase):
         configuration = Configuration.create_from_config_file()
         self.assertEqual(configuration.output, "myoutput_rc")
 
-    @mock.patch("lambda_package.Configuration.read_config_dict")
     def test_when_setup_file_has_output_and_rc_file_doesnt_then_config_has_setup_value(
         self, read_config_dict_mock: Mock
     ):
@@ -51,7 +49,6 @@ class ConfigurationTests(unittest.TestCase):
         configuration = Configuration.create_from_config_file()
         self.assertEqual(configuration.output, "myoutput_setup")
 
-    @mock.patch("lambda_package.Configuration.read_config_dict")
     def test_when_setup_file_has_output_and_rc_file_has_output_then_config_has_rc_value(
         self, read_config_dict_mock: Mock
     ):
@@ -66,7 +63,6 @@ class ConfigurationTests(unittest.TestCase):
         configuration = Configuration.create_from_config_file()
         self.assertEqual(configuration.output, "myoutput_rc")
 
-    @mock.patch("lambda_package.Configuration.read_config_dict")
     def test_when_config_file_contains_invalid_key_then_throw_exception(
         self, read_config_dict_mock: Mock
     ):
--- test/lambda_package_package.py
+++ test/lambda_package_package.py
@@ -7,24 +7,38 @@ from lambda_package import package
 from lambda_package.configuration import Configuration
 
 
+@mock.patch("lambda_package.lambda_package.find_paths")
+@mock.patch("lambda_package.lambda_package.zip_package")
+@mock.patch("lambda_package.Configuration.create_from_config_file")
+@mock.patch("lambda_package.lambda_package.read_gitignore")
+@mock.patch("lambda_package.lambda_package.build_requirements")
+@mock.patch("lambda_package.lambda_package.get_files_in_directory")
 class LambdaPackagePackageTests(unittest.TestCase):
     """
     Unit tests for the `lambda_package.package` function
     """
 
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_no_output_specified_then_zip_package_not_called(
-        self, zip_package_mock: Mock, find_paths_mock: Mock
+        self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         find_paths_mock.return_value = ("", "")
         package(configuration=Configuration())
         zip_package_mock.assert_not_called()
 
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_output_specified_then_zip_package_called(
-        self, zip_package_mock: Mock, find_paths_mock: Mock
+        self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         find_paths_mock.return_value = ([Path("mypaths")], "")
         package(configuration=Configuration(output="myoutput"))
@@ -32,10 +46,14 @@ class LambdaPackagePackageTests(unittest.TestCase):
             paths=[(Path("mypaths"), Path("mypaths"))], fp="myoutput"
         )
 
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_root_path_given_then_zip_package_called_with_relative_paths(
-        self, zip_package_mock: Mock, find_paths_mock: Mock
+        self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         find_paths_mock.return_value = ([Path("src/mypaths")], "")
         package(configuration=Configuration(output="myoutput"), root_path="src")
@@ -43,39 +61,42 @@ class LambdaPackagePackageTests(unittest.TestCase):
             paths=[(Path("src/mypaths"), Path("mypaths"))], fp="myoutput"
         )
 
-    @mock.patch("lambda_package.Configuration.create_from_config_file")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_config_not_given_then_read_from_disk(
         self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
         zip_package_mock: Mock,
         find_paths_mock: Mock,
-        create_from_config_file_mock: Mock,
     ):
         create_from_config_file_mock.return_value = Configuration()
         find_paths_mock.return_value = ([Path("mypaths")], "")
         package()
         create_from_config_file_mock.assert_called_once()
 
-    @mock.patch("lambda_package.Configuration.create_from_config_file")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_config_given_then_do_not_read_from_disk(
         self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
         zip_package_mock: Mock,
         find_paths_mock: Mock,
-        create_from_config_file_mock: Mock,
     ):
         create_from_config_file_mock.return_value = Configuration()
         find_paths_mock.return_value = ([Path("mypaths")], "")
         package(configuration=Configuration())
         create_from_config_file_mock.assert_not_called()
 
-    @mock.patch("lambda_package.lambda_package.read_gitignore")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_config_has_no_excludes_then_read_gitignore(
-        self, zip_package_mock: Mock, find_paths_mock: Mock, read_gitignore_mock: Mock
+        self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         read_gitignore_mock.return_value = ["gitignoreex"]
         find_paths_mock.return_value = ([Path("mypaths")], "")
@@ -84,11 +105,14 @@ class LambdaPackagePackageTests(unittest.TestCase):
         read_gitignore_mock.assert_called_once()
         find_paths_mock.assert_called_once_with(root_path=ANY, excludes=["gitignoreex"])
 
-    @mock.patch("lambda_package.lambda_package.read_gitignore")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_config_has_excludes_then_do_not_read_gitignore(
-        self, zip_package_mock: Mock, find_paths_mock: Mock, read_gitignore_mock: Mock
+        self,
+        get_files_in_directory_mock: Mock,
+        build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         read_gitignore_mock.return_value = []
         find_paths_mock.return_value = ([Path("mypaths")], "")
@@ -97,18 +121,14 @@ class LambdaPackagePackageTests(unittest.TestCase):
         read_gitignore_mock.assert_not_called()
         find_paths_mock.assert_called_once_with(root_path=ANY, excludes=["myexclude"])
 
-    @mock.patch("lambda_package.lambda_package.build_requirements")
-    @mock.patch("lambda_package.lambda_package.get_files_in_directory")
-    @mock.patch("lambda_package.lambda_package.read_gitignore")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_requirements_given_then_call_build_requirements(
         self,
-        zip_package_mock: Mock,
-        find_paths_mock: Mock,
-        read_gitignore_mock: Mock,
         get_files_in_directory_mock: Mock,
         build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         read_gitignore_mock.return_value = []
         find_paths_mock.return_value = ([Path("mypath")], "")
@@ -117,36 +137,28 @@ class LambdaPackagePackageTests(unittest.TestCase):
         package(configuration=Configuration(requirements="my_requirements"))
         build_requirements_mock.assert_called_once()
 
-    @mock.patch("lambda_package.lambda_package.build_requirements")
-    @mock.patch("lambda_package.lambda_package.get_files_in_directory")
-    @mock.patch("lambda_package.lambda_package.read_gitignore")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_requirements_not_given_then_do_not_call_build_requirements(
         self,
-        zip_package_mock: Mock,
-        find_paths_mock: Mock,
-        read_gitignore_mock: Mock,
         get_files_in_directory_mock: Mock,
         build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         read_gitignore_mock.return_value = []
         find_paths_mock.return_value = ([Path("mypaths")], "")
         package(configuration=Configuration())
         build_requirements_mock.assert_not_called()
 
-    @mock.patch("lambda_package.lambda_package.build_requirements")
-    @mock.patch("lambda_package.lambda_package.get_files_in_directory")
-    @mock.patch("lambda_package.lambda_package.read_gitignore")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_requirements_given_and_no_layer_output_given_then_add_files_to_main_zip(
         self,
-        zip_package_mock: Mock,
-        find_paths_mock: Mock,
-        read_gitignore_mock: Mock,
         get_files_in_directory_mock: Mock,
         build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         read_gitignore_mock.return_value = []
         find_paths_mock.return_value = ([Path("mypath1")], "")
@@ -182,18 +194,14 @@ class LambdaPackagePackageTests(unittest.TestCase):
             fp="my_output",
         )
 
-    @mock.patch("lambda_package.lambda_package.build_requirements")
-    @mock.patch("lambda_package.lambda_package.get_files_in_directory")
-    @mock.patch("lambda_package.lambda_package.read_gitignore")
-    @mock.patch("lambda_package.lambda_package.find_paths")
-    @mock.patch("lambda_package.lambda_package.zip_package")
     def test_when_requirements_given_and_layer_output_given_then_seperate_zip_created(
         self,
-        zip_package_mock: Mock,
-        find_paths_mock: Mock,
-        read_gitignore_mock: Mock,
         get_files_in_directory_mock: Mock,
         build_requirements_mock: Mock,
+        read_gitignore_mock: Mock,
+        create_from_config_file_mock: Mock,
+        zip_package_mock: Mock,
+        find_paths_mock: Mock,
     ):
         read_gitignore_mock.return_value = []
         find_paths_mock.return_value = ([Path("mypath1")], "")

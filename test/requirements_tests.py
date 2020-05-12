f96eb4b71eea0f4d20a385a20082056988738fe8
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-05-12 15:18:43 +0000

    Implemented local pip execution when use_docker is false

--- test/requirements_tests.py
+++ test/requirements_tests.py
@@ -7,23 +7,25 @@ from lambda_package.configuration import Configuration
 from lambda_package.requirements import TempDir, build_requirements
 
 
+@mock.patch("lambda_package.requirements.run")
 @mock.patch("pathlib.Path.mkdir")
 @mock.patch("pathlib.Path.unlink")
 @mock.patch("lambda_package.requirements.copy")
-@mock.patch("lambda_package.requirements.generate_temp_task_dir")
+@mock.patch("lambda_package.requirements.generate_temp_directory_name")
 @mock.patch("lambda_package.requirements.from_env")
 class RequirementsTests(unittest.TestCase):
     """
     General unit tests for the `requirements` module
     """
 
-    def test_when_requirements_given_and_use_docker_given_then_docker_is_called(
+    def test_when_requirements_given_and_use_docker_true_then_docker_is_called(
         self,
         from_env_mock: Mock,
         generate_temp_task_dir_mock: Mock,
         copy_mock: Mock,
         unlink_mock,
         mkdir_mock,
+        subprocess_run_mock,
     ):
         generate_temp_task_dir_mock.return_value = "my_temp_dir"
         run_mock = Mock()
@@ -50,6 +52,7 @@ class RequirementsTests(unittest.TestCase):
         copy_mock: Mock,
         unlink_mock,
         mkdir_mock,
+        subprocess_run_mock,
     ):
         generate_temp_task_dir_mock.return_value = "my_temp_dir"
         run_mock = Mock()
@@ -62,3 +65,25 @@ class RequirementsTests(unittest.TestCase):
         )
 
         self.assertEqual(res, expected_temp_dir)
+
+    def test_when_requirements_given_and_use_docker_false_then_local_pip_is_called(
+        self,
+        from_env_mock: Mock,
+        generate_temp_task_dir_mock: Mock,
+        copy_mock: Mock,
+        unlink_mock,
+        mkdir_mock,
+        subprocess_run_mock,
+    ):
+        generate_temp_task_dir_mock.return_value = "my_temp_dir"
+        expected_temp_dir = Path(TempDir).joinpath("my_temp_dir").absolute()
+
+        build_requirements(
+            Configuration(
+                requirements="my_requirements", use_docker=False, python_version="5.6"
+            )
+        )
+
+        subprocess_run_mock.assert_called_once_with(
+            ["pip", "install", "-t", expected_temp_dir, "-r", "my_requirements"]
+        )

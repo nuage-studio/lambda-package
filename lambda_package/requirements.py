719eb66c9b7da0135290a10b70e0506fbfd15297
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-05-12 15:18:43 +0000

    Implemented local pip execution when use_docker is false

--- lambda_package/requirements.py
+++ lambda_package/requirements.py
@@ -3,6 +3,7 @@ from random import choice
 from re import compile
 from shutil import copy
 from string import ascii_lowercase
+from subprocess import run
 from tempfile import gettempdir
 
 from docker import from_env
@@ -17,39 +18,62 @@ TempDir = gettempdir()
 VersionRegex = compile("(^[0-9]+\\.[0-9]+)(\\.[0-9]+)?$")
 
 
-def build_requirements(configuration: Configuration):
-    if configuration.use_docker:
-        # Create a temporary directory for /var/task
-        temp_dir = Path(TempDir).joinpath(generate_temp_task_dir()).absolute()
-        temp_dir.mkdir(parents=True)
-
-        # Copy the requirements file into the directory
-        requirements_src_path = Path(configuration.requirements)
-        requirements_dest_path = temp_dir.joinpath(requirements_src_path.name)
-        copy(str(requirements_src_path), str(requirements_dest_path))
-
-        # Launch the Docker instance to install the requirements
-        client = from_env()
-        vols = {str(temp_dir): {"bind": "/var/task", "mode": "z"}}
-        python_version = normalize_version(configuration.python_version)
-        client.containers.run(
-            f"lambci/lambda:build-python{python_version}",
-            f"pip install -t /var/task/ -r /var/task/{requirements_dest_path.name}",
-            volumes=vols,
-        )
-
-        # Remove the copied requirements file
-        requirements_dest_path.unlink()
-
-        return temp_dir
-
-    else:
-        raise Exception("Currently requirements can only built using docker")
-
-
-def generate_temp_task_dir():
+def build_requirements(configuration: Configuration) -> str:
     """
-    Generate a temporary directory name for the Lambda's `task` directory for Docker
+    Builds the `pip` requirements into a temporary directory, and returns a path
+    to that directory.  If `use_docker` is `True`, the requirements are built using
+    a Lambda Docker image.  Otherwise, pip will be called locally.
+    """
+    if configuration.use_docker:
+        return build_requirements_docker(configuration)
+    else:
+        return build_requirements_local(configuration)
+
+
+def build_requirements_docker(configuration: Configuration):
+
+    temp_dir = create_temp_requirements_directory()
+
+    # Copy the requirements file into the directory
+    requirements_src_path = Path(configuration.requirements)
+    requirements_dest_path = temp_dir.joinpath(requirements_src_path.name)
+    copy(str(requirements_src_path), str(requirements_dest_path))
+
+    # Launch the Docker instance to install the requirements
+    client = from_env()
+    vols = {str(temp_dir): {"bind": "/var/task", "mode": "z"}}
+    python_version = normalize_version(configuration.python_version)
+    client.containers.run(
+        f"lambci/lambda:build-python{python_version}",
+        f"pip install -t /var/task/ -r /var/task/{requirements_dest_path.name}",
+        volumes=vols,
+    )
+
+    # Remove the copied requirements file
+    requirements_dest_path.unlink()
+
+    return temp_dir
+
+
+def build_requirements_local(configuration: Configuration):
+    temp_dir = create_temp_requirements_directory()
+    print("temp dir ", temp_dir)
+    run(["pip", "install", "-t", temp_dir, "-r", configuration.requirements])
+    return temp_dir
+
+
+def create_temp_requirements_directory():
+    """
+    Create a temporary directory in which to install requirements so they can be zipped
+    """
+    temp_dir = Path(TempDir).joinpath(generate_temp_directory_name()).absolute()
+    temp_dir.mkdir(parents=True)
+    return temp_dir
+
+
+def generate_temp_directory_name():
+    """
+    Generate a temporary directory name
     """
     random_chars = "".join(choice(ascii_lowercase) for i in range(8))
     timestamp = datetime.now().strftime("%d%m%Y%H%M%S")
